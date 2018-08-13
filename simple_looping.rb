f8f7a0f04546c84c16f7a2be029b609a4891d4cf
Amy Zhou <aymeighz@gmail.com>
2018-08-13 17:05:50 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,4 +1,13 @@
-
+number_of_times = 0 
+def loop_iterator(number_of_times)
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+  loop do
+    number_of_times += 1 
+    puts phrase 
+    if number_of_times >= 7
+    break 
+  end
+end
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
